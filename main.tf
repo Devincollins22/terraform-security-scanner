2c98081527090dd8942bc5747151bb0a9655f974
Devin Collins <collin98@my.canisius.edu>
2026-04-28 18:20:24 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,12 +6,12 @@ resource "aws_security_group" "bad_sg" {
   description = "Allows SSH from a resticted network"
 
   ingress {
-    description = "Allow SSH from whole internet"
+    description = "Allow SSH from private indviduals"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
 
     # BAD PRACTICE: Open to the entire internet
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/24"]
   }
 }
